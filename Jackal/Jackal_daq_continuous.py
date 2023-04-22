281563322e27a86528ff82b85f8dfabcb52acd1e
Sanmeel Lagad <sanmeelvijay@vt.edu>
2023-04-21 22:51:59 -0400

    Updated formatting

--- Jackal/Jackal_daq_continuous.py
+++ Jackal/Jackal_daq_continuous.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 # Change directory to storage location
 
-
 # Sampling frequency and sampling rate
 freq = int(1.05e5)
 samplingRate = 1 / freq
@@ -19,7 +18,7 @@ BAUD = 115200  # Enter baud rate
 ser = serial.Serial(
     port=PORT,
     baudrate=BAUD,
-    timeout=1  # Change timeout if necessory
+    timeout=1  # Change timeout if necessary
 )
 ser.flush()
 
@@ -40,21 +39,21 @@ if __name__ == '__main__':
 
         ser.write(b"startCode\n")  # Data acquisition begins
 
-            while str(line2) != "Starting program in 2 seconds":
+        while str(line2) != "Starting program in 2 seconds":
 
-                print("Enter number of runs: \n")  # Provide number of runs
-                n = int(input())
+            print("Enter number of runs: \n")  # Provide number of runs
+            n = int(input())
 
-                for i in range(0, n):
-                    timestamp = datetime.now().strftime('%Y%m%d')[:-3]  # Timestamp
-                    time.sleep(2)
-                    FileName = timestamp + ".txt"  # Create File
+            for i in range(0, n):
+                timestamp = datetime.now().strftime('%Y%m%d')[:-3]  # Timestamp
+                time.sleep(2)
+                FileName = timestamp + ".txt"  # Create File
 
-                    with open(FileName, 'w') as file:
+                with open(FileName, 'w') as file:
 
-                        for j in range(0, 2 * freq):
-                            data = ser.readline().decode('utf-8').rstrip()
+                    for j in range(0, 2 * freq):
+                        data = ser.readline().decode('utf-8').rstrip()
 
-                            file.write(str(data) + '\n')
-                            time.sleep(samplingRate)
-    # ------------------------------Main loop above---------------------------------------
+                        file.write(str(data) + '\n')
+                        time.sleep(samplingRate)
+# ------------------------------Main loop above---------------------------------------
